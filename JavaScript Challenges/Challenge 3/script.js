6371201c1ff3e637552b88b6b331881a09182e59
bernadette <bernadette12ezghen@gmail.com>
2023-08-21 16:24:03 +0300

    challenge 3 done with part 3

--- JavaScript Challenges/Challenge 3/script.js
+++ JavaScript Challenges/Challenge 3/script.js
@@ -72,4 +72,27 @@ const squareNumberr = (number) => {
   
   console.log("Value of localVar outside squareNumber:", globalVar);
   console.log("Squared value:", squaredValue);
-  
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  let i = 10;
+
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+let ii = 1;
+
+do {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+  i++;
+} while (i <= 10);
+const favoriteFruits = ["apple", "banana", "strawberry", "kiwi", "orange", "watermelon"];
+
+for (const fruit of favoriteFruits) {
+  if (fruit.length > 5) {
+    console.log(fruit);
+  }
+}
