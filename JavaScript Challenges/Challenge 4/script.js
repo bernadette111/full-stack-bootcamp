be921656c981130c36a5dff3bb65a055c3fce95f
bernadette <bernadette12ezghen@gmail.com>
2023-08-22 15:50:05 +0300

    challenge 4 done with step2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,8 +1,18 @@
 const fiveNumbers = [ 1,2,3,4,5 ];
-const personalInfo{
-    name = "bernadette";
-    age = "18";
-    Height = "158";
-    location = "beirut";
-    nationality = "lebanese";
-}
+const personalInfo =[
+    name = "bernadette" ,
+    age = "18",
+    Height = "158",
+    location = "beirut",
+    nationality = "lebanese",
+]
+const nextThreeNumbers = [5 ,6 ,7];
+const allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+console.log(allNumbers);
+
+const additionalinfo =[
+    hobby = "reading" ,
+    education = "school",
+]
+const fullinfo =[...personalInfo, ...additionalinfo];
+console.log(fullinfo);
